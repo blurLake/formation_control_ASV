c27a5338aa23081e417f090b2c49d950c9d41796
Jieqiang Wei <jieqiang@UG.KTH.SE>
2018-11-15 12:03:10 +0100

    converge to a line

--- script.py
+++ script.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 
 
 b = 1.0
-c = 2.0
+c = 1.0
 d = 2.0
 y0 = [1,1,3,3,2,1]
-t = np.linspace(0, 100, 10000)
+t = np.linspace(0, 100, 100000)
 
 sol = odeint(three_agents, y0, t, args=(b, c, d))
 
